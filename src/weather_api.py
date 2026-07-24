a1954251560f3a368c109087359bb5b5e99f33b0
Areeba Irfan Jujara <aijujara@gmail.com>
2026-07-24 07:13:55 +0500

    Day 2 completed

--- src/weather_api.py
+++ src/weather_api.py
@@ -1,21 +1,74 @@
 import requests
 def get_coordinates(name):
     url = "https://geocoding-api.open-meteo.com/v1/search"
+
     extra = {
-        "city_name": name,
+        "name": name,
         "count": 1
-     }
+    }
 
-    response=requests.get(url,params=extra)
+    try:
+        response = requests.get(url, params=extra)
+        response.raise_for_status()
+        data = response.json()
 
-    if response.statuscode==200:
-        data=response.json()
-        if data["results"]:
-            return data["results"][0]["latitude"], data["results"][0]["longitude"]
-        else:
-            return None
+    except requests.RequestException as e:
+        print("network error:", e)
+        return None
+
+    print("API Response:", data)
+
+    if "results" in data:
+        return data["results"][0]["latitude"], data["results"][0]["longitude"]
+
+    else:
+        print(f"No results found for {name}")
+        return None
+    
 
 
 
 
+
+def get_weather(lat, lon):
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    info = {
+        "latitude": lat,
+        "longitude": lon,
+        "current": "temperature_2m"
+    }
+
+    try:
+        response = requests.get(url, params=info, timeout=10)
+        response.raise_for_status()
+
+        data = response.json()
+
+    except requests.RequestException as e:
+        print("Network error:", e)
+        return None
+
+    print("weather API responded with:", data)
+
+    if "current" in data:
+        weather = data["current"]["temperature_2m"]
+        unit = data["current_units"]["temperature_2m"]
+
+        print(f"Current weather is {weather} {unit}")
+        return weather, unit
+
+    else:
+        print("NO current weather data is found")
+        return None
+
+
+coordinates=get_coordinates("Karachi")
+              
+if coordinates:
+    lat,lon=coordinates
+    print(f"Fetching data for {lat},{lon}")
+    get_weather(lat,lon)
+else:
+    print("Could not fetch coordinates for the given location.")    
 
